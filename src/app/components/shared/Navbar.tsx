33eaeaf398f715e0a17f16ad797532d931dec513
unknown <kvitriseptia@gmail.com>
2026-03-05 09:25:23 +0700

    feat(auth): logout

--- src/app/components/shared/Navbar.tsx
+++ src/app/components/shared/Navbar.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { useRouter } from "next/navigation";
 
 interface NavbarProps {
     openSidebar: () => void;
 }
 
 export default function Navbar({ openSidebar }: NavbarProps) {
+    const router = useRouter();
     return (
         <header className="bg-white/80 backdrop-blur-xl sticky top-0 z-30 border-b border-gray-200/50">
             <div className="flex items-center justify-between px-4 lg:px-8 py-4">
@@ -165,6 +167,13 @@ export default function Navbar({ openSidebar }: NavbarProps) {
                             <div className="p-2 border-t border-gray-100">
                                 <a
                                     href="#"
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        localStorage.removeItem("token"); //untuk menghapus token
+                                        router.push("/login");
+                                    }
+
+                                    }
                                     className="flex items-center gap-3 px-3 py-2.5 text-sm text-red-600 hover:bg-red-50 rounded-xl transition-all"
                                 >
                                     <svg
@@ -180,7 +189,7 @@ export default function Navbar({ openSidebar }: NavbarProps) {
                                             d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
                                         />
                                     </svg>
-                                    Sign Out
+                                    Logout
                                 </a>
                             </div>
                         </div>
